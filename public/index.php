d940c12f1803f0e5493406d32564fc60718a4544
Гофман Юрий Александрович <gfmwebpost@gmail.com>
2026-02-25 11:04:26 +0500

    Контроллеры и почти роутер

--- public/index.php
+++ public/index.php
@@ -1,3 +1,21 @@
 <?php
 
-echo 'OK';
+declare(strict_types=1);
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+use App\Controller\ArticleController;
+use App\Controller\CategoryController;
+use App\Controller\HomeController;
+use App\Router;
+
+$router = new Router(
+    new HomeController(),
+    new CategoryController(),
+    new ArticleController(),
+);
+
+$uri = $_SERVER['REQUEST_URI'] ?? '/';
+$method = $_SERVER['REQUEST_METHOD'] ?? 'GET';
+
+echo $router->dispatch($uri, $method);
